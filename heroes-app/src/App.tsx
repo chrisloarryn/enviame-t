a4f298c2354919f35ef8c12b0f30d18b158c5578
Cristóbal Contreras <44170429+chrisloarryn@users.noreply.github.com>
2021-04-26 10:21:41 -0400

    feat: added temporal api

--- heroes-app/src/App.tsx
+++ heroes-app/src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Temporal } from 'proposal-temporal'
 
-function App() {
+const App = () => {
+  // const d = new Date().toISOString()
+  const dd = Temporal.now.instant().toZonedDateTimeISO('America/Santiago')
+  const day = `${dd.day}`.padStart(2, '0')
+  const month = `${dd.month}`.padStart(2, '0')
+  console.log(`${day}-${month}-${dd.year}`)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
