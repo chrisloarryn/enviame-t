109f52907379a085f96665e0eacc975d4c9a2bc1
chrisloarryn <chrisloarryn@gmail.com>
2021-04-27 04:46:45 -0400

    Update index.tsx

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -4,25 +4,27 @@ import { HeroI } from 'types/globals'
 import { Link } from 'wouter'
 import './Hero.css'
 
-interface HeroProps extends HeroI { }
+interface HeroProps extends HeroI {}
 
 const getDateInFormat = (date: Date): string => {
   const today = new Date(date)
   const year = today.getFullYear(),
     month = today.getMonth() + 1,
     day = today.getDate()
-  const paddedMonth = `${month}`.padStart(2, '0'), paddedDay = `${day}`.padStart(2, '0')
+  const paddedMonth = `${month}`.padStart(2, '0'),
+    paddedDay = `${day}`.padStart(2, '0')
   return `${year}-${paddedMonth}-${paddedDay}`
 }
 
 const Hero: FC<HeroProps> = ({ title, id, url, description, modified }) => {
-  
   const cleanText = description.replace(/<\/?[^>]+(>|$)/g, '')
   const date = getDateInFormat(modified)
   return (
     <div className='Hero'>
       <Link to={`/hero/${id}`} className='Hero-link'>
-        <h3>{title}<span>{date}</span></h3>
+        <h3>
+          {title} <hr /> <span>{date}</span>
+        </h3>
         <img loading='lazy' alt={title} src={url} />
         <h4>{cleanText}</h4>
       </Link>
