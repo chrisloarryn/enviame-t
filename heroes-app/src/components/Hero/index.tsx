6105b7876cb6a3c083e9b4ea6a786a687056dae0
chrisloarryn <chrisloarryn@gmail.com>
2021-04-27 10:59:33 -0400

    fix: no description in some heroes

--- heroes-app/src/components/Hero/index.tsx
+++ heroes-app/src/components/Hero/index.tsx
@@ -1,7 +1,8 @@
-import { Temporal } from 'proposal-temporal'
 import React, { FC } from 'react'
 import { HeroI } from 'types/globals'
 import { Link } from 'wouter'
+import * as R from 'ramda'
+
 import './Hero.css'
 
 interface HeroProps extends HeroI {}
@@ -19,6 +20,9 @@ const getDateInFormat = (date: Date): string => {
 const Hero: FC<HeroProps> = ({ title, id, url, description, modified }) => {
   const cleanText = description.replace(/<\/?[^>]+(>|$)/g, '')
   const date = getDateInFormat(modified)
+  const theDescription = !R.isEmpty(cleanText)
+    ? cleanText
+    : 'there is no description about this hero.'
   return (
     <div className='Hero'>
       <Link to={`/hero/${id}`} className='Hero-link'>
@@ -26,7 +30,7 @@ const Hero: FC<HeroProps> = ({ title, id, url, description, modified }) => {
           {title} <hr /> <span>{date}</span>
         </h3>
         <img loading='lazy' alt={title} src={url} />
-        <h4>{cleanText}</h4>
+        <h4>{theDescription}</h4>
       </Link>
     </div>
   )
